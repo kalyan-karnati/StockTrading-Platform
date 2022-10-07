8976c27c3e4060f411d67583ebe540963dc692a1
Mukunda Varma <varma13p@gmail.com>
2022-10-07 18:47:12 -0400

    Add Signup Form

---
+++ trading/auth/forms.py
@@ -0,0 +1,45 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, SelectField, IntegerField
+from wtforms.validators import DataRequired, Email, EqualTo, Length
+
+
+class SignupForm(FlaskForm):
+    """User Sign-up Form."""
+    name = StringField(
+        'Name',
+        validators=[DataRequired()]
+    )
+    username = StringField(
+        'User Name',
+        validators=[DataRequired()]
+    )
+    email = StringField(
+        'Email',
+        validators=[
+            Length(min=6),
+            Email(message='Enter a valid email.'),
+            DataRequired()
+        ]
+    )
+    password = PasswordField(
+        'Password',
+        validators=[
+            DataRequired(),
+            Length(min=6, message='Select a password with minimum 6 characters')
+        ]
+    )
+    confirm = PasswordField(
+        'Confirm Your Password',
+        validators=[
+            DataRequired(),
+            EqualTo('password', message='Passwords must match.')
+        ]
+    )
+    balance = IntegerField(
+        'Balance'
+    )
+    role = SelectField(
+        'Role',
+        choices=[('Admin', 'Admin'), ('User', 'User')]
+    )
+    submit = SubmitField('Register')
