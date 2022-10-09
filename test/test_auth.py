67ea680310f0c49b903eed3d16d004c8fe9743a2
kalyan-karnati <karnatik97@gmail.com>
2022-10-09 13:20:12 -0400

    Test Cases

--- test/test_auth.py
+++ test/test_auth.py
@@ -6,22 +6,18 @@ from trading import *
 from trading.auth.auth import *
 from trading.auth.models import *
 from trading.auth.forms import *
-from .test_engine import app
 from .test_engine import url
 
 class TestAuth(unittest.TestCase):
 
     def test_signup(self):
-        tester = app.test_client(self)
         response = requests.post(url+"/signup")
         self.assertEqual(response.status_code, 200)
     
     def test_login(self):
-        tester = app.test_client(self)
         response = requests.post(url+"/")
         self.assertEqual(response.status_code, 200)
     
     def test_logout(self):
-        tester = app.test_client(self)
         response = requests.post(url+"/logout")
         self.assertEqual(response.status_code, 200)
--- test/test_user.py
+++ test/test_user.py
@@ -6,13 +6,12 @@ from trading import *
 from trading.user.user import *
 from trading.user.models import *
 from trading.user.forms import *
-from .test_engine import app
 from .test_engine import url
 
 class TestUser(unittest.TestCase):
 
     def test_dashboard(self):
-        tester = app.test_client(self)
+         
         marketHoursData = {'startHour': '15:30', 'endHour': '12:30'}
         response = requests.post(url+"/userdashboard", data=marketHoursData)
         self.assertEqual(response.status_code, 200)
@@ -20,42 +19,42 @@ class TestUser(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
 
     def test_transaction_history(self):
-        tester = app.test_client(self)
+         
         response = requests.get(url+"/transaction_history")
         self.assertEqual(response.status_code, 200)
     
     def test_get_portfolio(self):
-        tester = app.test_client(self)
+         
         response = requests.get(url+"/get/portfolio")
         self.assertEqual(response.status_code, 200)
     
     def test_cancel_transaction(self):
-        tester = app.test_client(self)
+         
         response = requests.post(url+"/post/cancel_transaction/<tranid>")
         self.assertEqual(response.status_code, 200)
     
     def test_cash(self):
-        tester = app.test_client(self)
+         
         response = requests.post(url+"/cash")
         self.assertEqual(response.status_code, 200)
     
     def test_cashDeposit(self):
-        tester = app.test_client(self)
+         
         response = requests.post(url+"/cashDeposit")
         self.assertEqual(response.status_code, 200)
     
     def test_cashWithdraw(self):
-        tester = app.test_client(self)
+         
         response = requests.post(url+"/cashWithDraw")
         self.assertEqual(response.status_code, 200)
     
     def test_viewstocks(self):
-        tester = app.test_client(self)
+         
         response = requests.post(url+"/ViewStocks")
         self.assertEqual(response.status_code, 200)
     
     def test_buy_sell(self):
-        tester = app.test_client(self)
+         
         response = requests.post(url+"/buy_sell")
         self.assertEqual(response.status_code, 200)
 
